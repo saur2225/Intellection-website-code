0f79ec67d7882dc9db76c104908f4ab67f46ece7
Saurabh Srivastava <saurabhs@evertz.com>
2023-05-31 23:59:27 +0530

    pricing data

--- src/app/itl-pricing/itl-pricing.component.ts
+++ src/app/itl-pricing/itl-pricing.component.ts
@@ -16,9 +16,13 @@ export class ItlPricingComponent implements OnInit {
         "Memories",
         "5 webinars",
         "2 psychological sessions"],
-        perYear: 1500,
+        perYear: 12000,
         type: 'month',
-        fullPrice: 18000
+        fullPrice: 18000,
+        offPrice: 6000,
+        cutYear:18000,
+        downOne: 1000,
+        downTwo: 1500,
     },
     {
       year: '3',
@@ -27,10 +31,14 @@ export class ItlPricingComponent implements OnInit {
         "Health Module",
         "Co-curricular Module",
         "Memories",
-        "15/25 webinars",
-        "psychological sessions"],
-        perYear: 7000,
-        fullPrice: 21000
+        "15 webinars",
+        "6 psychological sessions"],
+        perYear: 24000,
+        fullPrice: 21000,
+        offPrice: 12000,
+        cutYear:36000,
+        downOne: 8000,
+        downTwo: 12000,
     },
     {
       year: '5',
@@ -39,10 +47,14 @@ export class ItlPricingComponent implements OnInit {
         "Health Module",
         "Co-curricular Module",
         "Memories",
-        "15/25 webinars",
-        "psychological sessions"],
-        perYear: 6000,
-        fullPrice: 30000
+        "25 webinars",
+        "10 psychological sessions"],
+        perYear: 30000,
+        fullPrice: 30000,
+        offPrice: 18000,
+        cutYear:50000,
+        downTwo: 10000,
+        downOne: 6000,
     }
   ]
   constructor() { }
